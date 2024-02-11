e09a7ee7ee245b4980edbb70060cbd0865a731ec
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-02-10 22:12:13 -0800

    impl ButtonWidget custom widget

---
+++ lib/helpers/widget/button_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget(
+      {super.key,
+      required this.onPress,
+      required this.text,
+      this.primary = true,
+      this.width = double.infinity,
+      this.height = 47,
+      this.radius = 12,
+      });
+  final Function() onPress;
+  final String text;
+  final bool primary;
+  final double width;
+  final double height;
+  final double radius;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+        onPressed: onPress,
+        minWidth: width,
+        height: height,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(radius)
+        ),
+        color: primary? context.theme.colorScheme.primary : context.theme.colorScheme.secondary,
+        child: Text(text,style: context.textTheme.bodyMedium!.apply(color: Colors.white)),
+    );
+  }
+}
--- lib/modules/auth/pages/start_page.dart
+++ lib/modules/auth/pages/start_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_telegram_clone/helpers/widget/button_widget.dart';
+import 'package:flutter_telegram_clone/helpers/widget/sized_widget.dart';
 import 'package:get/get.dart';
 
 class StartPage extends StatelessWidget {
@@ -9,12 +11,18 @@ class StartPage extends StatelessWidget {
     return Scaffold(
       body: SizedBox(
         width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Spacer(),
-            Image.asset(context.isDarkMode? "assets/images/logo_dark.png" :"assets/images/logo_light.png")
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(),
+              Image.asset(context.isDarkMode? "assets/images/logo_dark.png" :"assets/images/logo_light.png"),
+              H(MediaQuery.sizeOf(context).height * 0.08),
+              // buttons
+              ButtonWidget(onPress: () {}, text: "ثبت نام",primary: false),
+            ],
+          ),
         ),
       ),
     );
