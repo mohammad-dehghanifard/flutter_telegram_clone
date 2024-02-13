48c94c2bfe87fb30027c816d3a5a9f637b23da14
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-02-13 00:18:07 -0800

    impl save token and load token Function in utilsMethod

--- lib/helpers/utils/utils_method.dart
+++ lib/helpers/utils/utils_method.dart
@@ -1,3 +1,5 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 bool validateIranPhoneNumber(String value) {
   const String pattern = r'^(\+98|0)?9\d{9}$';
   RegExp regExp = RegExp(pattern);
@@ -5,4 +7,19 @@ bool validateIranPhoneNumber(String value) {
     return true;
   }
   return false;
+}
+
+Future<void> saveToken(String value) async {
+  final SharedPreferences preferences = await SharedPreferences.getInstance();
+  await preferences.setString("token", value);
+}
+
+Future<String?> getToken() async {
+  final SharedPreferences preferences = await SharedPreferences.getInstance();
+  final token = preferences.getString("token");
+  if(token != null){
+    return token;
+  } else {
+    return null;
+  }
 }
