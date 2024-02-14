35ac620bcc9bc90c87049ed4f283e40a50879991
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-02-13 21:35:49 -0800

    impl ChatRepository class and getUserInfoApi function

---
+++ lib/backend/repositories/chat_repository.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_telegram_clone/backend/models/user.dart';
+import 'package:flutter_telegram_clone/backend/repositories/base_repository.dart';
+
+class ChatRepository extends BaseRepository {
+  // Get user Info Api
+  Future<User?> getUserInfoApi() async {
+    final response = await dio.get('/auth/profile');
+    validateResponse(response);
+    print(response);
+    return response.data['user'];
+  }
+}
