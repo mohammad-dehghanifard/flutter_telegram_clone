914b16a8fb1466853630eb74ed81683a31c50b51
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-02-13 22:17:46 -0800

    impl _fetchConversation in HomeController

--- lib/modules/home/controllers/home_controller.dart
+++ lib/modules/home/controllers/home_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_telegram_clone/backend/models/conversation.dart';
 import 'package:flutter_telegram_clone/backend/repositories/chat_repository.dart';
 import 'package:flutter_telegram_clone/helpers/utils/base_controller.dart';
 import 'package:flutter_telegram_clone/helpers/utils/user_helper.dart';
@@ -5,7 +6,7 @@ import 'package:flutter_telegram_clone/helpers/utils/user_helper.dart';
 class HomeController extends BaseController {
 //====================== variable ==============================================
   final ChatRepository _repository = ChatRepository();
-
+  List<Conversation>? conversationList;
 //====================== method ================================================
   Future<void> _fetchUserInfo() async {
     final result = await _repository.getUserInfoApi();
@@ -14,10 +15,14 @@ class HomeController extends BaseController {
     }
 
   }
-
+  Future<void> _fetchConversation() async {
+    conversationList = await _repository.getAllConversationApi();
+    update();
+  }
 //======================= life cycle ===========================================
   @override
   void onInit() {
+    _fetchConversation();
     _fetchUserInfo();
     super.onInit();
   }
