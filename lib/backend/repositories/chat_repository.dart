77c647be3bd64f4d4e7b7f585b5e2fb57ad77264
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-02-13 22:15:50 -0800

    impl getAllConversationApi in ChatRepository

--- lib/backend/repositories/chat_repository.dart
+++ lib/backend/repositories/chat_repository.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_telegram_clone/backend/models/conversation.dart';
 import 'package:flutter_telegram_clone/backend/models/user.dart';
 import 'package:flutter_telegram_clone/backend/repositories/base_repository.dart';
+import 'package:flutter_telegram_clone/backend/response/conversation_response.dart';
 
 class ChatRepository extends BaseRepository {
   // Get user Info Api
@@ -7,5 +9,11 @@ class ChatRepository extends BaseRepository {
     final response = await dio.get('/auth/profile');
     validateResponse(response);
     return User.fromJson(response.data['user']);
+  }
+  // Get conversation Api
+  Future<List<Conversation>> getAllConversationApi() async {
+    final response = await dio.get('/conversations');
+    validateResponse(response);
+    return ConversationResponse.fromJson(response.data).data ?? [];
   }
 }
