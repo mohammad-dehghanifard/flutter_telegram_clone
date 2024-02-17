fd5f9adcb3776296f01eadc1225a3f75c35159d7
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-02-17 01:26:31 -0800

    impl createConversationApi method in ChatRepository class

--- lib/backend/repositories/chat_repository.dart
+++ lib/backend/repositories/chat_repository.dart
@@ -2,6 +2,7 @@ import 'package:flutter_telegram_clone/backend/models/conversation.dart';
 import 'package:flutter_telegram_clone/backend/models/message.dart';
 import 'package:flutter_telegram_clone/backend/models/user.dart';
 import 'package:flutter_telegram_clone/backend/repositories/base_repository.dart';
+import 'package:flutter_telegram_clone/backend/requests/conversation_request.dart';
 import 'package:flutter_telegram_clone/backend/response/conversation_response.dart';
 import 'package:flutter_telegram_clone/backend/response/message_response.dart';
 import 'package:flutter_telegram_clone/backend/response/user_response.dart';
@@ -30,5 +31,12 @@ class ChatRepository extends BaseRepository {
     final response = await dio.get("/conversation/$id/messages");
     validateResponse(response);
     return MessageResponse.fromJson(response.data).data ?? [];
+  }
+  // Create Conversation
+  Future<Conversation> createConversationApi({required ConversationRequest request}) async {
+    final data = await request.data();
+    final response = await dio.post("/create/conversation",data: data);
+    validateResponse(response);
+    return Conversation.fromJson(response.data["conversation"]);
   }
 }
