5fe6bd1b6e42f51ccfbfde0059ab11904ddc92c3
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-02-18 22:26:18 -0800

    refactor CircleIconButtonWidget widget

--- lib/helpers/widget/circle_icon_button_widget.dart
+++ lib/helpers/widget/circle_icon_button_widget.dart
@@ -3,23 +3,33 @@ import 'package:get/get.dart';
 
 class CircleIconButtonWidget extends StatelessWidget {
   const CircleIconButtonWidget({
-    super.key, required this.icon, required this.onTap,
+    super.key,
+    required this.icon,
+    required this.onTap,
+    this.width = 38,
+    this.height = 38,
+    this.iconSize = 18,
+    this.bgColor
   });
   final IconData icon;
   final Function() onTap;
+  final double width;
+  final double height;
+  final double iconSize;
+  final Color? bgColor;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        width: 38,
-        height: 38,
+        width: width,
+        height: height,
         decoration: BoxDecoration(
             shape: BoxShape.circle,
-            color: context.theme.colorScheme.secondaryContainer
+            color: bgColor ?? context.theme.colorScheme.secondaryContainer
         ),
-        child:  Icon(icon,size: 18),
+        child:  Icon(icon,size: iconSize),
       ),
     );
   }
